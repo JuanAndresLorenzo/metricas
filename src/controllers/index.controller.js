b52becf8abf03e02634ea1095ec947182ec7939d
Juan Lorenzo <juanandreslorenzo01@gmail.com>
2023-07-14 12:11:59 -0300

    Apis

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -43,9 +43,63 @@ const deleteTask = async (req, res) => {
     });
 }
 
+const getActivity = async (req, res) => {
+    const response = await pool.query('SELECT * FROM public."Activity"');
+    res.status(200).json(response.rows);
+};
+
+const getActivityByDateRange = async (req, res) => {
+    const dateFrom = req.params.dateFrom;
+    const dateTo = req.params.dateTo;
+    const response = await pool.query('SELECT * FROM public."Activity" WHERE  activity_start_date_time >= $1 and activity_end_date_time <= $2', [dateFrom, dateTo]);
+    res.json(response.rows);
+};
+
+const createActivity = async (req, res) => {
+    const { task_id } = req.body;
+
+    const response = await pool.query('INSERT INTO public."Activity" (activity_start_date_time, task_id) VALUES ($1, NOW())', [task_id]);
+    console.log(response);
+    res.json({
+        message: 'task created succesfully',
+        body: {
+            task: {start_time, task_id}
+        }
+    });
+};
+
+const setFinishTime  = async (req, res) => {
+    const { id } = req.body;
+
+    const response = await pool.query('UPDATE public."Activity" SET activity_end_date_time = NOW() WHERE activity_id = $1', [id]);
+    console.log(response);
+    res.json({
+        message: 'task created succesfully',
+        body: {
+            task: {id}
+        }
+    });
+};
+
+const deleteActivity = async (req, res) => {
+    const id = req.params.id;
+    const response = await pool.query('DELETE FROM public."Task" WHERE task_id = $1', [id]);
+    res.json({
+        message: 'task deleted succesfully',
+        body: {
+            task: {id}
+        }
+    });
+}
+
 module.exports = {
     getTasks,
     getTaskById,
     createTasks,
-    deleteTask
+    deleteTask,
+    getActivity,
+    setFinishTime,
+    createActivity,
+    deleteActivity,
+    getActivityByDateRange   
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,15 @@
 const {Router} = require('express');
 const router = Router();
-const {getTasks, createTasks, getTaskById, deleteTask} = require("../controllers/index.controller")
+const { getTasks, getTaskById, createTasks, deleteTask, getActivity, setFinishTime, createActivity, deleteActivity, getActivityByDateRange } = require("../controllers/index.controller")
 
 router.get('/tasks', getTasks)
 router.get('/tasks/:id', getTaskById)
 router.post('/tasks', createTasks)
 router.delete('/tasks/:id', deleteTask)
+router.get('/activity' , getActivity)
+router.put('/activity/:id' , setFinishTime)
+router.post('/activity' , createActivity)
+router.delete('/activity/:id' , deleteActivity)
+router.get('/activity' , getActivityByDateRange)
 
 module.exports = router;
